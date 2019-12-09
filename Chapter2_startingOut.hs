68d626d88b12c53c808ada2304fc0ec89f881861
Ho Duy <40297016+hoDuyAPCS@users.noreply.github.com>
2019-12-09 20:51:49 +0700

    Update Chapter2_startingOut.hs

--- Chapter2_startingOut.hs
+++ Chapter2_startingOut.hs
@@ -1,2 +1,44 @@
-{- Multiline Comment    
+{- Multiline Comment
+-- Single Line Comment
+-}
+
+--SOME ARITHMETIC OPERATION
+
+{-
+ghci> 3 + 1
+4
+The plus sign can be replace with - * /
+ghci> 3 * (3-1)
+6
+-}
+
+{-
+This is bad
+ghci> 3 + -5
+Fix : 3 + (-5)
+-}
+
+--BOOLEAN ALGEBRA
+{-
+&& : AND
+|| : OR
+not 
+== : EQUAL
+/= : DIFFER
+-}
+
+--FUNCTION
+{-
+ghci> succ 8
+9
+ghci> min 1 2
+1
+ghci> max 2 3
+3
+ghci> succ(succ 9)
+11
+-}
+
+{-
+doubleMe x = x + x
 -}
